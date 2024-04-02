4dabe103ee5d096c1ec0dfc865d385cda81fc0ae
marcos-dev14 <mprogramador.10@gmail.com>
2024-04-02 09:05:11 -0300

    aula 1 - finalizada

---
+++ src/app/_layout.tsx
@@ -0,0 +1,27 @@
+import "@/styles/global.css"
+
+import { Slot } from "expo-router"
+
+import { Loading } from "@/components/loading"
+
+import {
+  useFonts,
+  Inter_700Bold,
+  Inter_500Medium,
+  Inter_400Regular
+} from "@expo-google-fonts/inter"
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    useFonts,
+    Inter_700Bold,
+    Inter_500Medium,
+    Inter_400Regular
+  })
+
+  if (fontsLoaded) {
+    return <Loading />
+  }
+
+  return <Slot />
+}
